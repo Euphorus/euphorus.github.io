b5c47950feed712a542a2521c65b5c4d25c78750
Yuvraj Singh Kumpavat <115008153+Euphorus@users.noreply.github.com>
2024-07-30 05:00:02 +0100

    some update

---
+++ js/commands.js
@@ -0,0 +1,121 @@
+var youtube = "https://youtube.com/@euphorus1?feature=shared"
+var linkedin = "https://www.linkedin.com/in/yuvraj-singh-kumpavat-4436a8223/";
+var twitch = "https://www.twitch.tv/euphorus1"
+var github = "https://github.com/Euphorus";
+var sudo = "https://www.youtube.com/watch?v=TVVum6X3TuM";
+var email = "mailto:syuvraj.2021@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm Yuvraj!👋</span>",
+  "<br>",
+  "<li>🔭 I’m currently a 3rd year undergrad, pursuing BSc in Games Design and Programming.</li>",
+  "<li>🌱 My fields of intetest are</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Mathematics, Physics
+        - Programming, Level Designing
+        - Skateboarding, Racing</pre>`,
+  `<li> I love...
+   *Try using a social command.`,
+  `<li> 👨‍💻 Check out my github profile to view my projects
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=syuvraj.2021@gmail.com">syuvraj.2021@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' +
+    youtube +
+    '" target="blank">youtube.com/euphorus1' +
+    "</a>",
+    "<br>",
+  'twitch        <a href="' +
+    twitch +
+    '" target="blank">twitch.tv/euphorus1' +
+    "</a>",
+    "<br>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="blank">linkedin/yuvrajKumpavat' +
+    "</a>",
+    "<br>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/Euphorus' +
+    "</a>",
+  
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  `<a href="https://euphorus.itch.io/guffawing" target="blank"><span class='underline'>GuffaWing</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">A 2D RPG game built during Global Games Jam 2024
+
+Built With
+
+    *Unity
+    *C#
+    *Github
+    *Maximum Effort of 3 people.
+`,
+
+  "<br>",
+
+  `<a href="https://euphorus.itch.io/climate-crusaders" target="blank"><span class='underline'>Climate Crusaders</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">This game is about energy conservation set in the 80s. This project was a part of Staffs Summer Research which went fron July 2023 to August 2023.
+This game is playable in the browser, do give it a go!
+
+Built With
+
+    *Bitsy.com
+    *3 hours of sleep
+    *with no sow to reap
+    *trying not to be a deadbeat
+    *by making my drink neat
+    *How am I going to make a name
+    *when I got no game!
+`,
+
+  "<br>",
+
+  `<a href="https://euphorus.itch.io/the-invertor" target="blank"><span class='underline'>The Invertor</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">This game was by the far the best platformer I ever made in my life. Instead of jumping, the gravity flips and the player is hanging upside down.
+  
+Built With
+
+    *Unity
+    *C#
+    *Constant support my lecturers
+    *2 hours of staring constantly at the code on why the flip-gravity mechanic wasn't working!
+`,
+
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
